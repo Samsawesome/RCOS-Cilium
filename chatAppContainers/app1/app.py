d67b0220b32b02be7d24b92eebc4fa66babfb56e
Samsawesome <samsawesome2@gmail.com>
2025-04-20 13:10:46 -0400

    now logs timestamp when message recieved

--- chatAppContainers/app1/app.py
+++ chatAppContainers/app1/app.py
@@ -28,14 +28,17 @@ def send_message():
     data = request.json
     message = data.get("message")
     sender_id = data.get("sender_id")  # Track sender to avoid infinite loop
+    timestamp = data.get("timestamp")
 
     if message:
         if sender_id == 'app1':
+            print("App 1 sent message at", timestamp) # log message with timestamp in console for error checking
             local_messages.append(message)  # Add the message to local container
             # Send the message to the other container (App2) with a sender ID of 'app1'
             requests.post("http://app3:5002/send_message", json={"message": message, "sender_id": "app1"})
             return (jsonify({"status": "Message sent successfully"}), 200)
         elif sender_id == 'app2':
+            print("App 2 sent message at", timestamp) # log message with timestamp in console for error checking
             # If the sender is 'app2', prevent adding the message again in app1
             #if message not in messages:  # Check if the message is already in local messages
             messages.append(message)  # Add the message to local container
