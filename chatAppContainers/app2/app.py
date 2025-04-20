87a41f196714e4d36ff2b347f35131c0bcdb5a00
Samsawesome <samsawesome2@gmail.com>
2025-04-20 13:24:38 -0400

    fixed missing timestamp when sending to other app

--- chatAppContainers/app2/app.py
+++ chatAppContainers/app2/app.py
@@ -35,7 +35,7 @@ def send_message():
             print("App 2 sent message at", timestamp) # log message with timestamp in console for error checking
             local_messages.append(message)  # Add the message to local container
             # Send the message to the other container (App2) with a sender ID of 'app1'
-            requests.post("http://app3:5002/send_message", json={"message": message, "sender_id": "app2"})
+            requests.post("http://app3:5002/send_message", json={"timestamp": timestamp, "message": message, "sender_id": "app2"})
             return (jsonify({"status": "Message sent successfully"}), 200)
         elif sender_id == 'app1':
             print("App 1 sent message at", timestamp) # log message with timestamp in console for error checking
